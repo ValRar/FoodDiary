c164373ca79316507108d18d2c5da49b97cd1872
ValRar <val21219@gmail.com>
2024-09-29 10:44:17 +0300

    try to fix dates render

--- frontend/src/components/utilities.ts
+++ frontend/src/components/utilities.ts
@@ -1,7 +1,7 @@
 export function displayDate(date: Date): string {
-  let minutes = date.getUTCMinutes().toString();
+  let minutes = date.getMinutes().toString();
   minutes = minutes.length == 2 ? minutes : "0" + minutes;
-  let hours = date.getUTCHours().toString();
+  let hours = date.getHours().toString();
   hours = hours.length == 2 ? hours : "0" + hours;
   return hours + ":" + minutes;
 }
