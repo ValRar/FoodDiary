c4c011fe22fd103eab2f17326b7b65ed8f879d5c
ValRar <val21219@gmail.com>
2024-10-06 15:33:28 +0300

    little dark theme bug fix

--- frontend/public/scripts/detectColorTheme.js
+++ frontend/public/scripts/detectColorTheme.js
@@ -1,13 +1,12 @@
 function detectColorTheme() {
   if (document.cookie.includes("show_dark_theme")) return;
+  const cookieExpirationDate = new Date(Date.now() + 2628e9).toUTCString();
   if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-    document.cookie = "show_dark_theme=1";
+    document.cookie = "show_dark_theme=1; expires=" + cookieExpirationDate;
     const html = document.getElementsByTagName("html")[0];
     html.classList.add("dark");
   } else {
-    let cookieExpirationDate = new Date(Date.now() + 2628e9);
-    document.cookie =
-      "show_dark_theme=0; expires=" + cookieExpirationDate.toUTCString();
+    document.cookie = "show_dark_theme=0; expires=" + cookieExpirationDate;
   }
 }
 
